c969a2ffa131a6f7692ae6816743f3f3be73c7fe
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2022-07-10 08:52:21 +0200

    Initial implementation

--- actions/pr-linker/src/main.ts
+++ actions/pr-linker/src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core';
-import { getOctokit } from '@actions/github';
+// import { getOctokit } from '@actions/github';
 import artifact from '@actions/artifact';
 import { readdir } from 'fs/promises';
 import path from 'path';
-import commentForOctokit from './comment';
+// import commentForOctokit from './comment';
 
 const getAlbatrouzFiles = async (dir: string): Promise<string[]> => {
   const files = await readdir(dir);
@@ -12,18 +12,17 @@ const getAlbatrouzFiles = async (dir: string): Promise<string[]> => {
 };
 
 async function run(): Promise<void> {
-  const commitHash = core.getInput('commit', { required: true });
-  const repoToken = core.getInput('repo-token', { required: true });
+  // const commitHash = core.getInput('commit', { required: true });
+  // const repoToken = core.getInput('repo-token', { required: true });
   const albatrouzOutDir = core.getInput('albatrouz-out', { required: true });
 
-  const octokit = getOctokit(repoToken);
-  const comments = commentForOctokit(octokit);
+  // const octokit = getOctokit(repoToken);
+  // const comments = commentForOctokit(octokit);
 
   const artifacts = artifact.create();
   const responses = await Promise.all((await getAlbatrouzFiles(albatrouzOutDir)).map((f) => (
     artifacts.uploadArtifact(path.basename(f), [f], albatrouzOutDir)
   )));
-  
 
   responses.forEach((uploadResponse) => {
     if (uploadResponse.failedItems.length > 0) {
